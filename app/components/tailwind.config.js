e178c3a8961688b2ad45686cf5285c442124a954
DeltaDivingNZ <47905975+DeltaDivingNZ@users.noreply.github.com>
2026-02-02 19:26:03 +1300

    Create tailwind.config.js

---
+++ app/components/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        delta: {
+          blue: "#006bb3",
+          green: "#009957",
+          dark: "#111827",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
